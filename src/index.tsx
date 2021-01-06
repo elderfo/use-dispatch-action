4682bd13b4daee566eb94712c002d94b26fe9826
Chris Getsfred <cgetsfred@gmail.com>
2021-01-05 20:11:28 -0600

    Move types into main package

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
 import { Dispatch, useCallback } from 'react';
-import { Action, DispatchAction } from './types';
+
+/**
+ * Defines an action and optionally it's payload
+ * @template T Name of the action
+ * @template P (Optional) type for the payload
+ */
+export type Action<T extends string = string, P = never> = { type: T } & ([
+  P
+] extends [never]
+  ? {}
+  : { payload: P });
+
+export type DispatchAction<T> = [T] extends [never]
+  ? () => void
+  : (payload: T) => void;
 
 export function useDispatchAction<
   A extends Action,
